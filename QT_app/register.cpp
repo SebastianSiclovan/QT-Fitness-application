15d2dfd9013a9f2eb5478fa68a15bc6509f12a85
siclo <siclovansebastian@yahoo.com>
2023-06-26 21:45:31 +0300

    Update register with modify some functions, new feature to redirect user to login

--- QT_app/register.cpp
+++ QT_app/register.cpp
@@ -6,14 +6,19 @@ Register::Register(QWidget *parent) :
     ui(new Ui::Register)
 {
     ui->setupUi(this);
-    this->setWindowTitle("Register");
+    this->setWindowTitle("Sebastian's app - Register");
 }
 
 Register::~Register()
 {
     delete ui;
+    qDebug() << "Destructorul a fost apelat";
 }
-
+/*
+    password_security: function which reiceive password parameter and verify if password have at least one number and one uppercase
+    -Return true if password have at least one number and one uppercase
+    -Return false if password don t have ...
+ */
 bool Register::password_security(QString &password)
 {
     int cnt_digits = 0;
@@ -48,18 +53,39 @@ bool Register::password_security(QString &password)
     }
 }
 
+/*
+    get_warning: function which receive a warning parameter and verify if a warning has occured
+    -Return true if is a warning
+    -Return false if isn't
+ */
+bool Register::get_warining(bool warning)
+{
+    if (warning == true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+
+}
+
+
 bool Register::check_registerFields()
 {
-    bool warning_field = false;
+    this->warning_field = false;
 
-    QString username = ui->lineEdit_userName->text();
-    QString firstname = ui->lineEdit_firstName->text();
-    QString lastname = ui->lineEdit_lastName->text();
-    QString email = ui->lineEdit_email->text();
-    QString password = ui->lineEdit_password->text();
+    username = ui->lineEdit_userName->text();
+    firstname = ui->lineEdit_firstName->text();
+    lastname = ui->lineEdit_lastName->text();
+    email = ui->lineEdit_email->text();
+    password = ui->lineEdit_password->text();
+
 
     // verify if fields is empty
-    if (username.isEmpty() )
+    if (username.isEmpty())
     {
         QMessageBox::warning(this, "Register", "Please complete the username field");
         warning_field = true;
@@ -107,10 +133,40 @@ bool Register::check_registerFields()
 
 void Register::on_pushButton_registerBtn_clicked()
 {
+    dB_obj = new Database_application();
+    dB_obj->databaseInit();
+
     if(check_registerFields() == false)
     {
+        data_insertByUser = {username, firstname, lastname, email, password};
+
         QMessageBox::information(this, "Register", "Succesfully register, you will be redirected to login section");
+        dB_obj->database_addUserData_atRegister(data_insertByUser);
+
+        hide();
+        redirect_toLogin->show();
+
+
     }
+    else
+    {
+        if (get_warining(this->warning_field == false))
+        {
+            QMessageBox::information(this, "Register", "Something went wrong");
+        }
+
+
+    }
+
+}
+
+
+void Register::on_pushButton_Login_clicked()
+{
+    redirect_toLogin = new Login();
+    hide();
+    redirect_toLogin->show();
+
 
 }
 
--- QT_app/register.h
+++ QT_app/register.h
@@ -1,14 +1,25 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
+#include "database_application.h"
+
 #include <QDialog>
 #include <QMessageBox>
+#include <QtSql>
+#include <QDebug>
+
+#include "Login.h"
+
+#include "Interfaces/Register_interface.h"
+
+#include <list>
+
 
 namespace Ui {
 class Register;
 }
 
-class Register : public QDialog
+class Register : public QDialog, public Register_interface
 {
     Q_OBJECT
 
@@ -17,12 +28,41 @@ public:
     ~Register();
 
 private slots:
-    bool check_registerFields();
-    bool password_security(QString & obj);
+
     void on_pushButton_registerBtn_clicked();
+    void on_pushButton_Login_clicked();
 
 private:
+    bool get_warining(bool warning);
+    bool check_registerFields() override;
+    bool password_security(QString & obj) override;
+private:
     Ui::Register *ui;
+
+    Login * redirect_toLogin = new Login();
+
+    bool warning_field;
+
+
+
+
+    // database variables
+    QSqlQuery query;
+    QSqlDatabase db;
+    Database_application * dB_obj;
+
+    // atributes for register fields
+    QString username;
+    QString firstname;
+    QString lastname;
+    QString email;
+    QString password;
+
+    // store data insert by user
+    std::list <QString> data_insertByUser;
+
+
+
 };
 
 #endif // REGISTER_H
