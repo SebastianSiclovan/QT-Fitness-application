cf8a3393c1e930b1669f1e3d71f36fa76d1d1a06
siclo <siclovansebastian@yahoo.com>
2023-07-15 15:01:44 +0300

    Functional implementation of a BMI calculator, BMI and weight category display

--- QT_app/bmi_calculator.cpp
+++ QT_app/bmi_calculator.cpp
@@ -13,3 +13,79 @@ BMI_calculator::~BMI_calculator()
 {
     delete ui;
 }
+
+float BMI_calculator::calculate_BMI(float height, float weight)
+{
+    float result_BMI = weight / (height*height);
+
+    return result_BMI;
+
+}
+
+QString BMI_calculator::set_weightCategory(float BMI)
+{
+    QString category;
+
+    if (BMI < 18.5)
+    {
+        category = "Underweight";
+    }
+    else
+    {
+        if (BMI > 25)
+        {
+            category = "Overweight";
+        }
+        else
+        {
+            category = "Normal weight";
+        }
+    }
+
+    return category;
+
+}
+
+
+bool BMI_calculator::check_BMIcalculator_Fields()
+{
+    this->warning_field = false;
+
+    this->weight = ui->lineEdit_weight->text();
+    this->height = ui->lineEdit_height->text();
+
+    if(weight.isEmpty())
+    {
+        QMessageBox::warning(this, "BMI_calculator", "Please fill in the weight");
+        warning_field = true;
+    }
+
+    if (height.isEmpty())
+    {
+        QMessageBox::warning(this, "BMI_calculator", "Please fill in the height");
+        warning_field = true;
+    }
+
+    return warning_field;
+
+}
+
+
+void BMI_calculator::on_checkCategory_btn_clicked()
+{
+    single_obj = new BMI_calculator();
+
+
+
+    if(check_BMIcalculator_Fields() == false)
+    {
+        QMessageBox::information(this, "BMI_calculator", "Your category and BMI are now available!");
+        float BMI = single_obj->calculate_BMI(this->height.toFloat(), this->weight.toFloat());
+        QString weightCategory = single_obj->set_weightCategory(BMI);
+        ui->label_setBMI->setText(QString::number(BMI));
+        ui->label_setCategory->setText(weightCategory);
+
+    }
+
+}
+
--- QT_app/bmi_calculator.h
+++ QT_app/bmi_calculator.h
@@ -2,6 +2,8 @@
 #define BMI_CALCULATOR_H
 
 #include <QDialog>
+#include <QMessageBox>
+#include <QDebug>
 
 namespace Ui {
 class BMI_calculator;
@@ -15,8 +17,25 @@ public:
     explicit BMI_calculator(QWidget *parent = nullptr);
     ~BMI_calculator();
 
+private slots:
+    void on_checkCategory_btn_clicked();
+
+    bool check_BMIcalculator_Fields();
+
+    float calculate_BMI(float height, float weight);
+
+    QString set_weightCategory(float BMI);
+
+
+
 private:
     Ui::BMI_calculator *ui;
+
+    bool warning_field;
+    QString weight;
+    QString height;
+
+    BMI_calculator * single_obj;
 };
 
 #endif // BMI_CALCULATOR_H
